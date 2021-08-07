8c7cfed263ea76e7a2a64feac98754d975041852
Neil Tambe <neiltambe@valtryek.local>
2021-08-07 13:14:09 -0700

    My August calendar I did with Ojas Freind/Teacher

---
+++ KhanAcademy/calendar.js
@@ -0,0 +1,40 @@
+var x = 71;
+var y = 49;
+var r = 0;
+var side = 36;
+for( var i=1; i<=7; i++) {
+    rect(x, y, side, side, r);
+    x = x + side;
+}
+
+
+var x = 71;
+var y = y + side;
+for( var i=1; i<=7; i++) {
+    rect(x, y, side, side, r);
+    x = x + side;
+}
+
+// 3rd row
+var x = 71;
+var y = y + side;
+for(var i=1; i<=7; i++) {
+    rect(x, y, side, side, r);
+    x = x + side;
+}
+
+var x = 71;
+var y = y + side;
+for( var i=1; i<=7; i++) {
+    rect(x, y, side, side, r);
+    x = x + side;
+}
+
+var x = 71;
+var y = y + side;
+for( var i=1; i<=3; i++) {
+    rect(x, y, side, side, r);
+    x = x + side;
+}
+
+
