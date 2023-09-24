1e45b9033a054f9be9def6ae79570dcd69656aa2
Neil Tambe <neiltambe@valtryek.local>
2023-09-23 17:38:11 -0700

    add if condition

--- js/letter_blocks.js
+++ js/letter_blocks.js
@@ -5,8 +5,8 @@ function println(x) {
   console.log(x);
 }
 var A = [
-  [0,1,1,1,0],
-  [1,0,0,0,1],
+  [0,0,1,0,0],
+  [0,1,0,1,0],
   [1,0,0,0,1],
   [1,1,1,1,1],
   [1,0,0,0,1],
@@ -15,7 +15,11 @@ var A = [
 ];
 for (var i = 0; i < A.length; i++) {
   for (var j = 0; j < A[0].length; j++) {
-    print(A[i][j]);  
+    if (A[i][j] == 0) {
+      print(" ");
+    } else {
+      print("1");
+    }
   }
-  println("");
+  println(" ");
 }
