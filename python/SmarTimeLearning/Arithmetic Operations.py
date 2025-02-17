70a886493f30a96651089f7bd42184879cd51131
Neil Tambe <88286429+neiltambe@users.noreply.github.com>
2025-02-16 16:58:10 -0800

    Arithmetic Operations

---
+++ python/SmarTimeLearning/Arithmetic Operations.py
@@ -0,0 +1,7 @@
+number1 = int(input("number1: "))
+number2 = int(input("number2: " ))
+
+print("Addition: ", number2, "+", number1, "=", number2 + number1)
+print("Subtraction: ", number2, "-", number1, "=", number2 - number1)
+print("Multiplication: ", number2, "*", number1, "=", number1 * number2)
+print("Division: ", number2, "/", number1, "=", number2 / number1)
